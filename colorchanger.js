14ed72b193029c1f7ae715ab3597013b48ff354d
zohaibhassan9 <zabaidafashion@gmail.com>
2025-01-19 19:31:42 +0500

    have sucessfully created Background Color Changer App

--- colorchanger.js
+++ colorchanger.js
@@ -1,3 +1,22 @@
 const colorCode = document.getElementById('colorCode');
 const changeColorCodeBtn = document.getElementById('changeColorBtn');
 
+
+
+function getRandomColor(){
+    const letters = '0123456789ABCDEF';
+    let color  = '#';
+
+for (let i = 0; i < 6; i++){
+    color += letters[Math.floor(Math.random()*16)];
+}
+return color;
+}
+
+
+changeColorCodeBtn.addEventListener('click', ()=>{
+const randomColor = getRandomColor();
+
+document.body.style.backgroundColor = randomColor;
+colorCode.textContent = randomColor;
+})
