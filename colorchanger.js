ecb81453fb65b4b17e35a89e2c3ff9eefebb4699
zohaibhassan9 <zabaidafashion@gmail.com>
2025-01-19 19:32:15 +0500

    Spacing

--- colorchanger.js
+++ colorchanger.js
@@ -4,11 +4,11 @@ const changeColorCodeBtn = document.getElementById('changeColorBtn');
 
 
 function getRandomColor(){
-    const letters = '0123456789ABCDEF';
-    let color  = '#';
+const letters = '0123456789ABCDEF';
+let color  = '#';
 
 for (let i = 0; i < 6; i++){
-    color += letters[Math.floor(Math.random()*16)];
+color += letters[Math.floor(Math.random()*16)];
 }
 return color;
 }
